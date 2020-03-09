811ec5945dbe239b256edde159b82f9e29bbfc66
Dan Ichim <dan-constantin.ichim@endava.com>
2020-03-09 11:07:49 +0200

    add sample

---
+++ readme.md
@@ -0,0 +1,9 @@
+```
+npm init
+entry: src/index.html
+npm install parcel-bundler --save-dev
+"start": "parcel src/index.html --open",
+"build": "parcel build src/index.html"
+npm install react
+npm install react-dom
+```
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World from JSHeroes community! 📦 🚀</h1>
+      </div>
+    )
+  }
+}
