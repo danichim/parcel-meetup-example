0bffe5788103a1b192dfea1e7242b03ea48537cf
Dan Ichim <dan-constantin.ichim@endava.com>
2020-03-10 12:42:45 +0200

    change order css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './App'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
 import '@fortawesome/fontawesome-free/css/all.css'
+import App from './App'
 
 render(<App />, document.getElementById('app'))
